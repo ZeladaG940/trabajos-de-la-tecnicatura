bb8fab91dec8975b3f3ebc2a8e422350f6b954ba
Orlando zelada gira <zeladagiraorlando@gmail.com>
2025-05-27 18:26:09 -0300

    vemos los condiconales en python

---
+++ python/clase-6/control-de-flujo.py
@@ -0,0 +1,5 @@
+condicon = True
+if condicon == True:#si la condion es verdadera se ejecutara este bloque
+    print("condicion verdadera")
+else:
+    print("consicion falsa")#si la concion es falsa ejercutara esto
