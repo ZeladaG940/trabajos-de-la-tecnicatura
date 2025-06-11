bbed4942956722bd3746ac29990a97691b0a5c26
orlando zelada <orlandozeladagira@gmail.com>
2025-06-12 00:52:04 +0200

    ingrese la clase de js

---
+++ javascript/clase-8-VarLetCons/yaNoSeUtilizaVar.js
@@ -0,0 +1,10 @@
+//ya no se utilizavar
+var cosa = "hola";
+console.log(cosa);
+
+let algo = "como estas";//se puede cambiar su valor y tipo nada mas que si se crea en un bloque
+console.log(algo);//esa variable queda ahi nomas
+
+const pedro = "pepito"; // no se puede cambiar el dato y su tipo
+console.log(pedro);
+
